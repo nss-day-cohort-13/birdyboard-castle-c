ae82d915f647f439fb1b77af38d9fe70ba8ba1d2
Castle Crawford <nathanscottcrawford@gmail.com>
2016-08-02 10:34:59 -0500

    user and screen name tests currently passing, user data is being serial/deserialized and currently producing expected output

--- board.py
+++ board.py
@@ -1,8 +1,15 @@
+import pickle
+import sys
+import uuid
 
 class Board():
 
-def show_menu(self):
+  def __init__(self):
+    self.uData = {}
+    self.chirpData = {}
 
+
+  def show_menu(self):
 
     print('what would you like to do?')
     print("1. New User Account")
@@ -13,39 +20,90 @@ def show_menu(self):
     print("6. Exit")
     user_choice = input(">")
 
-
-
     if user_choice == "1":
+      self.deserializeUser()
       print("Create a New Account to get chirpy!.")
-      inp = (input("Enter full name >" )
-      inp1 = (input("Enter screen name >" )
+      inp1 = input("Enter screen name: " )
+      self.uData.setdefault("username:", []).append(inp1)
+      inp2 = input("Enter full name: ")
+      self.uData.setdefault("fullname:", []).append(inp2)
+      self.uData.setdefault("ID:", []).append(uuid.uuid4())
+      self.serializeUser()
+      print(self.uData)
 
     if user_choice == "2":
-      print("Select a User.")
-      inp = (input(">")
-
-
-    if user_choice == "3":
-      print("Time to get chirpy! Select any chirp to get chirpin'")
-      inp = (input(">")
-
-
-    if user_choice == "4":
-      print("Time to get chirpy in public! Select any public chirp to get chirpin'")
-      inp = (input(">")
-
-
-    if user_choice == "5":
-      print("Time to get chirpy in private! Select any private chirp to get chirpin'")
-      inp = (input(">")
-
-
-    if user_choice != "6":
-      self.show_menu()
+      self.deserializeUser()
+      print("Select a User to chirp it up with!.")
+      self.usernameSelect()
+      inp = int(input(">"))
+      self.loadUser(inp)
 
 
 
 
+    # if user_choice == "3":
+    #   print("Time to get chirpy! Select any chirp to get chirpin'.")
+    #   inp = input(">")
+
+
+    # if user_choice == "4":
+    #   print("Send a new public chirp!")
+    #   inp = input(">")
+
+
+    # if user_choice == "5":
+    #   print("Send a new private chirp!")
+    #   inp = input(">")
+
+
+    # if user_choice != "6":
+    #   self.show_menu()
+  # def serializeChirp(self):
+  #   with open('chirps.txt', 'wb+') as u:
+  #     pickle.dump(self.chirpData, u)
+
+  # def deserializeChirp(self):
+  # try:
+  #   with open('chirps.txt', 'rb+') as u: #rb in read binary
+  #     self.chirpData = pickle.load(u)
+
+  # except: EOFError
+
+
+  def serializeUser(self):
+    with open('userData.txt', 'wb+') as u:
+      pickle.dump(self.uData, u)
+
+  def deserializeUser(self):
+    try:
+      with open('userData.txt', 'rb+') as u: #rb in read binary
+        self.uData = pickle.load(u)
+
+    except: EOFError
+
+  def usernameSelect(self):
+    d = self.uData
+    users = d['username:']
+    for k, v in enumerate(users, start = 1):
+      print(k,v)
+      return (k,v)
+
+  def fullnameSelect(self):
+    newL = []
+    d = self.uData
+    users = d['fullname:']
+    for k, v in enumerate(users, start = 1):
+      print (k,v)
+      return (k,v)
+
+
+  # def loadUser(self, inp):
+  #   d = self.uData
+  #   users = d['username:']
+  #   for k, v in enumerate(users, start = 1):
+  #     if k == inp:
+  #       return k
+  #       print('hi')
 
 
 
--- testBoard.py
+++ testBoard.py
@@ -1,8 +1,10 @@
 import unittest
 from board import *
+import uuid
 
 class TestBoard(unittest.TestCase):
 
+# http://i.imgur.com/lZ0bxod.gifv  <--- uber lolz
 
 # 1. New User Account
 # 2. Select User
@@ -10,31 +12,32 @@ class TestBoard(unittest.TestCase):
 # 4. Public Chirp
 # 5. Private Chirp
 # 6. Exit
-
+  @classmethod
   def setUp(self):
     self.board = Board()
+    self.board.deserializeUser()
 
 
-  def test_user_ID(self):
-    self.assertEqual(self.board.create_ID(1), 1)
+  # def test_user_ID(self):
+    # self.assertEqual(self.board.create_ID(1), 1)
 
   def test_user_full_name(self):
-    self.assertEqual(self.board.create_full_name("castle crawford"), "castle crawford")
+    self.assertEqual(self.board.fullnameSelect(), (1, 'castle crawford'))
 
-   def test_user_screen_name(self):
-    self.assertEqual(self.board.create_screen_name("RooR"),"RooR" )
-
-
-  def test_user_select(self):
-    self.assertEqual(self.board.select_user("RooR"), "RooR")
+  def test_user_screen_name(self):
+    self.assertEqual(self.board.usernameSelect(), (1, 'RooR'))
 
 
-  def test_chirp_public(self):
-    self.assertEqual(self.board.public_chirp("RooR", "Rawr", "Roar"), "RooR", "Rawr", "Roar")
+  # def test_user_select(self):
+  #   self.assertEqual(self.board.select_user("RooR"), "RooR")
 
 
-  def test_chirp_private(self):
-    self.assertEqual(self.board.private_chirp("RooR", "Rawr"), "RooR", "Rawr")
+  # def test_chirp_public(self):
+  #   self.assertEqual(self.board.public_chirp("RooR", "Rawr", "Roar"), "RooR", "Rawr", "Roar")
+
+
+  # def test_chirp_private(self):
+  #   self.assertEqual(self.board.private_chirp("RooR", "Rawr"), "RooR", "Rawr")
 
 
 
